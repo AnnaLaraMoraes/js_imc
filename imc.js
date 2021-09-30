e73c6e19ca0efe17a0c0ebeda15b0c5e6e4f43fe
Anna Lara <annalara1426@gmail.com>
2021-09-30 21:51:05 +0200

    add calculo imc

---
+++ imc.js
@@ -0,0 +1,15 @@
+/*Atividade referente a aula  7 da disciplina de Gerência de Configuração.
+Esta atividade deve ser entregue até 24 de setembro de 2021.
+
+Nesta atividade vamos fazer uma atividade prática. 
+Instale o git em seu computador
+Inicialize um repositório
+Realize a configuração do repositório
+Crie um programa em uma linguagem de programação de sua preferência que calcule o IMC(Índice de Massa Corpórea)
+Crie uma versão de seu código
+Para avaliação desta atividade envie os log de seu repositório */
+
+const peso = 65
+const altura = 1.63
+const calculoImc = peso/(altura * altura)
+console.log(calculoImc)
